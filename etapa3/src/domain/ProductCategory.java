566bc2a173940a0c6396d4dcdb3e531a2e1bb8f9
eiuahiue <ouihsdiaud>
2019-05-30 11:59:36 -0300

    productcategory test

--- etapa3/src/domain/ProductCategory.java
+++ etapa3/src/domain/ProductCategory.java
@@ -1,5 +1,22 @@
 package domain;
 
 public class ProductCategory {
-
+	
+	private String name;
+	
+	public ProductCategory(String str)
+	{
+		name = str;
+	}
+	
+	public void setName(String str)
+	{
+		name = str;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
 }
--- etapa3/src/domain/ProductCategoryTest.java
+++ etapa3/src/domain/ProductCategoryTest.java
@@ -4,5 +4,30 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class ProductCategoryTest {
+	
+	ProductCategory cat;
+	String testName = "Shampoo";
+	
+	@Before
+	public void setUp()
+	{
+		cat = new ProductCategory(testName);
+	}
+	
+	@Test
+	public void testSetName()
+	{
+		String newName = "Cream";
+		cat.setName(newName);
+		
+		assertTrue(cat.getName() == newName);
+	}
+	
+	@Test
+	public void testGetName()
+	{
+		assertTrue(cat.getName() == testName);
+	}
+	
 
 }
