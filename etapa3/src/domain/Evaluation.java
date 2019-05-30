a22a40ebe908067bf0a822765e0a7fbe9c131dff
eiuahiue <ouihsdiaud>
2019-05-30 11:45:51 -0300

    evaluation test

--- etapa3/src/domain/Evaluation.java
+++ etapa3/src/domain/Evaluation.java
@@ -1,5 +1,33 @@
 package domain;
 
 public class Evaluation {
+	
+	private int score = 0;
+	private boolean done;
+	
+	public boolean isDone()
+	{
+		return done;
+	}
+	
+	public void setScore(int sc)
+	{
+		score = sc;
+		done = true;
+	}
 
+	public int getScore()
+	{
+		return score;
+	}
+	
+	public Evaluation()
+	{
+		
+	}
+	
+	public Evaluation(EvaluationGroup group, Product product, User reviewer)
+	{
+		
+	}
 }
--- etapa3/src/domain/EvaluationTest.java
+++ etapa3/src/domain/EvaluationTest.java
@@ -4,5 +4,39 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class EvaluationTest {
+	
+	EvaluationGroup evalGroup;
+	Product product;
+	User reviewer;
+	Evaluation eval;
 
+	@Before
+	public void setUp()
+	{
+		evalGroup = new EvaluationGroup();
+		product = new Product();
+		reviewer = new User();
+		eval = new Evaluation(evalGroup, product, reviewer);
+	}
+
+	/*
+	 * A avaliação está compĺeta quando score é
+	 * diferente de zero?
+	 */
+	@Test
+	public void testIsDone()
+	{
+		assertFalse(eval.isDone());
+		
+		eval.setScore(3);
+		assertTrue(eval.isDone());
+	}
+	
+	@Test
+	public void testSetScore()
+	{
+		int grade = 5;
+		eval.setScore(grade);
+		assertTrue(eval.getScore() == grade);
+	}
 }
